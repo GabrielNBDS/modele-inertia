a24fdf62b8f1e84065a26d88cb618968c0d46078
GabrielNBDS <gabriel.nbds@gmail.com>
2024-04-24 12:34:58 -0400

    add modele config

--- app/modules/auth/routes.ts
+++ app/modules/auth/routes.ts
@@ -1,3 +1,4 @@
+import modele from '#config/modele'
 import { middleware } from '#start/kernel'
 import router from '@adonisjs/core/services/router'
 const LogoutController = () => import('./use_cases/logout/controller.js')
@@ -5,19 +6,21 @@ const ForgotPasswordController = () => import('./use_cases/forgot_password/contr
 const LoginController = () => import('./use_cases/login/controller.js')
 const RegisterController = () => import('./use_cases/register/controller.js')
 
-router.get('/entrar', [LoginController, 'view']).middleware(middleware.guest())
-router.post('/login', [LoginController, 'handle'])
+if (modele.auth.login) {
+  router.get('/entrar', [LoginController, 'view']).middleware(middleware.guest())
+  router.post('/login', [LoginController, 'handle'])
 
-router.post('logout', [LogoutController, 'handle']).use(middleware.auth())
+  router.post('logout', [LogoutController, 'handle']).use(middleware.auth())
 
-router.get('/cadastrar', [RegisterController, 'view']).middleware(middleware.guest())
-router.post('/register', [RegisterController, 'handle']).middleware(middleware.guest())
+  router
+    .get('/esqueci-minha-senha', [ForgotPasswordController, 'view'])
+    .middleware(middleware.guest())
+  router.post('/esqueci-minha-senha', [ForgotPasswordController, 'sendEmail'])
+  router.post('/esqueci-minha-senha/codigo', [ForgotPasswordController, 'handle'])
+  router.post('/esqueci-minha-senha/trocar-senha', [ForgotPasswordController, 'changePassword'])
+}
 
-router
-  .get('/esqueci-minha-senha', [ForgotPasswordController, 'view'])
-  .middleware(middleware.guest())
-router.post('/esqueci-minha-senha', [ForgotPasswordController, 'sendEmail'])
-router.post('/esqueci-minha-senha/codigo', [ForgotPasswordController, 'handle'])
-router.post('/esqueci-minha-senha/trocar-senha', [ForgotPasswordController, 'changePassword'])
-
-router.on('/email-link').renderInertia('auth/magic-link')
+if (modele.auth.signUp) {
+  router.get('/cadastrar', [RegisterController, 'view']).middleware(middleware.guest())
+  router.post('/register', [RegisterController, 'handle']).middleware(middleware.guest())
+}
---
+++ config/modele.ts
@@ -0,0 +1,6 @@
+export default {
+  auth: {
+    login: true,
+    signUp: false,
+  },
+}
